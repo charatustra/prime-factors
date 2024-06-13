7434818f91856321a1c493b766364c0b7924dc8e
jwoong23.seo <jwoong23.seo@samsung.com>
2024-06-13 09:40:32 +0900

    [feature] 1

--- test_prime_factor.py
+++ test_prime_factor.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 
+from prime_factor import PrimeFactor
+
 
 class TestPrimeFactor(TestCase):
-    def test_of(self):
-        self.fail()
+    def test_prime_factor_of_1(self):
+        prime_factor = PrimeFactor()
+        self.assertEqual([], prime_factor(1))
