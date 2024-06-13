0af156d14b5eb8efaa56014ce7069f93bdfd3297
jwoong23.seo <jwoong23.seo@samsung.com>
2024-06-13 09:42:55 +0900

    [feature] 2

--- test_prime_factor.py
+++ test_prime_factor.py
@@ -6,4 +6,8 @@ from prime_factor import PrimeFactor
 class TestPrimeFactor(TestCase):
     def test_prime_factor_of_1(self):
         prime_factor = PrimeFactor()
-        self.assertEqual([], prime_factor(1))
+        self.assertEqual([], prime_factor.of(1))
+
+    def test_prime_factor_of_2(self):
+        prime_factor = PrimeFactor()
+        self.assertEqual([2], prime_factor.of(2))
